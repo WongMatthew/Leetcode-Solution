dd53cccc6afcb6d3bfdc4bfece1885aadd098fff
Matt Wong <wongmatthew357@gmail.com>
2022-12-23 13:44:39 -0800

    q14

---
+++ 14/solution.py
@@ -0,0 +1,23 @@
+# beats 97% of runtime and 98% of memory
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        strs.sort()
+        prefix = strs[0]
+        print(prefix)
+
+        # iterate through 1st string
+        for i, c in enumerate(prefix):
+            # i >= len(strs[-1])  --> breaks b/c the string is too short compared to size of prefix
+            # strs[-1][i] != c  --> check if current value is in the same position as the one its being compared to
+            if i >= len(strs[-1]) or strs[-1][i] != c:
+                prefix = prefix[:i]
+                break
+        return prefix
