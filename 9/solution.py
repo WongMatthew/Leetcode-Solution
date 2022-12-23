af303be7fda46e85eaf57b55df2d9eddc34a7be8
Matt Wong <wongmatthew357@gmail.com>
2022-12-23 12:53:47 -0800

    q9

---
+++ 9/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        elif x < 10:
+            return True
+        x_rev = list(str(x))
+        x_rev.reverse()
+        if list(str(x)) == x_rev:
+            return True
+        return False    
