7464ffb7680c8d37ea6608aef61341eb9d9592c8
Matt Wong <wongmatthew357@gmail.com>
2022-12-31 20:48:11 -0800

    q22

---
+++ 22/solution.py
@@ -0,0 +1,22 @@
+# beats 98% runtime and 68% memory
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        res = []
+        dfs(0, 0, '')
+        return res
