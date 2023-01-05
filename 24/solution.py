b2d002eb4dd6237c83e6415c6c593b8d56d28128
Matt Wong <wongmatthew357@gmail.com>
2023-01-04 23:27:31 -0800

    q24

---
+++ 24/solution.py
@@ -0,0 +1,13 @@
+# 
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
