c3f39dba6d2a1ede49fec1c416c8871d591c2573
Matt Wong <wongmatthew357@gmail.com>
2023-01-09 00:19:38 -0800

    add comments to q32

--- 32/solution.py
+++ 32/solution.py
@@ -11,14 +11,19 @@ class Solution(object):
             return 0
 
         max_length = 0
-        stack=[-1]
+        stack=[-1] # stack to store '('
+        # loop through string
         for i in range(len(s)):
             if s[i] == '(':
+                # push '(' onto stack
                 stack.append(i)
+            # if s[i] = ')' remove '(' from stack
             else:
                 stack.pop()
+                # if stack is empty, push current index onto stack
                 if not stack:
                     stack.append(i)
+                # update max length
                 else:
                     max_length=max(max_length, i-stack[-1])
         return max_length
