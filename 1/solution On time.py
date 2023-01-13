1f724e3308bebdca1b821b443b5a704abe1c9982
Matt Wong <wongmatthew357@gmail.com>
2023-01-12 21:46:43 -0800

    add comments

--- 1/solution On time.py
+++ 1/solution On time.py
@@ -2,9 +2,16 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
 
+            # Iterate through the array of numbers
         for index in range(0, len(nums)):
+            # Calculate the difference between the target and the current element
             diff = target - nums[index]
+            
+            # Check if the difference is already in the hash map
             if hash_map.get(diff) == None:
+                # If not, add the current element and its index to the hash map
                 hash_map[nums[index]] = index
             else:
+                # If it is, return the indices of the current element and the element in the hash map
+                # that add up to the target
                 return [index, hash_map[diff]]
