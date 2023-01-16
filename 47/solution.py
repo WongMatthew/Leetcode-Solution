4e38b2a29bc54d57c8ba28dad41de212dc222ddf
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-16 13:56:43 -0800

    q47

---
+++ 47/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        perm, res = [], []
+        count = {n:0 for n in nums}
+
+        for n in nums:
+            count[n] += 1
+        
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm[:])
+                return
+            
+            for n in count:
+                if count[n] > 0:
+                    count[n] -= 1
+                    perm.append(n)
+                    dfs()
+                    perm.pop()
+                    count[n] += 1
+
+        dfs()
+        return res
