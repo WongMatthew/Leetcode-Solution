40c6618e681f49529df805ddea91b73205a9ba5f
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-16 13:57:33 -0800

    update comments

--- 47/solution.py
+++ 47/solution.py
@@ -2,13 +2,18 @@
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        # set up permutation stack and results list
         perm, res = [], []
+        # set up keys for dictionary
         count = {n:0 for n in nums}
 
+        # add values to dictionary
         for n in nums:
             count[n] += 1
-        
+
+        # depth first search
         def dfs():
+            # break out when finished
             if len(perm) == len(nums):
                 res.append(perm[:])
                 return
