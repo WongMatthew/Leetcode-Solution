0352ea71f92154b9bbd87a8dbcfb66aa0cafd98f
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-16 14:46:05 -0800

    add comments q66

--- 66/solution.py
+++ 66/solution.py
@@ -1,10 +1,17 @@
+# beats 93% runtime and 96% memory
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # initialize empty string
         string = ""
+        # iterate through digits
         for num in digits:
+            # add each digit to string
             string += str(num)
-
+        
+        # convert string to int and add 1
         res = int(string)+1
 
+        # convert int to string and then to list
         final_string = [int(d) for d in str(res)]
         return final_string
